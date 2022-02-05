e2961172d2ff4c3b1ceccd01a51e5b815f6fcfb4
DanielLinux <danielcarvalhorst@gmail.com>
2022-02-04 23:38:30 -0300

    Feat - Sign-up page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,17 @@
+import { Frame } from "./style"
+import { Link } from "react-router-dom"
 export default function RegisterPage(){
     return(
         <>
+        <Frame>
             <h1>MyWallet</h1>
+            <input type="text" placeholder="Nome"/>
             <input type="email" placeholder="E-mail"/>
             <input type="password" placeholder="Senha" />
-            <button>Entrar</button>
-            <Link to="/sign-up"><a href="#">Primeira vez? Cadastre-se!</a></Link>
+            <input type="password" placeholder="Confirme a senha" />
+            <button>Cadastrar</button>
+            <Link to="/sign-in"><a href="#">Já tem uma conta? Entre agora!</a></Link>
+        </Frame>
         </>
     )
 }
