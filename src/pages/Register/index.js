3004443a534354c7fc52ae6a74b7937846cdba68
DanielLinux <danielcarvalhorst@gmail.com>
2022-02-04 23:35:49 -0300

    Feat - Sign-in page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,11 @@
 export default function RegisterPage(){
     return(
         <>
-        <h1>BROSJ</h1>
+            <h1>MyWallet</h1>
+            <input type="email" placeholder="E-mail"/>
+            <input type="password" placeholder="Senha" />
+            <button>Entrar</button>
+            <Link to="/sign-up"><a href="#">Primeira vez? Cadastre-se!</a></Link>
         </>
     )
 }
